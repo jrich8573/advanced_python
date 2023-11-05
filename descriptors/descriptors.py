8d6e439fbc429d7d03c3e9f0bdeea53812a4acf0
Jason Rich <jasonrich85@icloud.com>
2023-11-04 21:29:17 -0400

    added descriptors

--- descriptors/descriptors.py
+++ descriptors/descriptors.py
@@ -1,6 +1,7 @@
 # Descriptors are created to manage the attribute of different classes which use the object as reference
 # provide a way to write reusable code to share between classes
-# Descriptors are a powerful, general purpose protocol. They are the mechanism behind properties, methods, static methods, class methods, and super() . They are used throughout Python itself. 
+# Descriptors are a powerful, general purpose protocol. They are the mechanism behind properties, methods, static methods, class methods, and super().
+# They are used throughout Python itself. 
 # Descriptors simplify the underlying C code and offer a flexible set of new tools for everyday Python programs.
 # Descriptors are the interface for the __get__(), __set__(), __delete__()
 # to intialize a descriptor: decr.__method__(self, obj, type=None)-> value
@@ -16,3 +17,67 @@
 # set: sets a value of the attribute. It doesn't return anything. Can raise the attribute error exception
 # delete:delete operation on an attribute
 # value: the value of the attribute
+
+class Descriptors: 
+    def __init__(self):
+        self.__bmi = 0
+    
+    def __get__(self, instance, owner):
+        return self.__bmi
+    
+    def __set__(self, instance, value):
+        if isinstance(value, int):
+            print(value)
+        else: 
+            raise TypeError("BMI can only be an int")
+        
+        if value < 0:
+            raise ValueError("BMI can never be less then 0")
+        
+        self.__bmi = value
+        
+    def __delete__(self, instance):
+        del self._bmi
+        
+        
+class Person: 
+     # contructor function that is called only once when the object is created
+     # That means that is we run person1.name = 10, the conditions in the constructor 
+     # will not catch the error. That is, if we change any class attribute after it is intially created
+     # the type checking in the constructor will not catch the errors. 
+    
+    bmi = Descriptors() 
+    def __init__(self, name, age, bmi):
+        self.name = name
+        self.age = age
+        self.bmi = bmi
+        #  if isinstance(self.name, str):
+        #      print(self.name)
+        #  else:
+        #      raise ValueError("Name of the person can never be an integer")
+        #  if self.bmi < 0: # to catch negative values
+        #      raise ValueError("BMI can not be less than 0")
+     
+     # string interprelation         
+     
+    def __str__(self):
+         return "{0} age is {1} with bmi of {2}".format(self.name, self.age, self.bmi)
+    
+ 
+ 
+person1 = Person("John", "28", 23)
+print(person1)
+person2 = Person("James", "25", 48)
+print(person2)
+print(person1)
+'''
+23
+James age is 25 with bmi of 23
+48
+James age is 25 with bmi of 48
+James age is 25 with bmi of 48
+
+We see this behavior because the descriptor is associated with the class and not the instance.
+Therefore, given that we passed a new bmi parameter to the class, in the person2 variable, 
+we have simply overwritten our previously pass bmi parameter. 
+'''
