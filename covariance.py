812586bf7390d100c9a069a59168e131bbd6e8b1
jrich8573 <jasonrich85@icloud.com>
2023-11-19 19:56:28 -0500

    edited covariance

--- covariance.py
+++ covariance.py
@@ -3,19 +3,7 @@ import math
 import statistics
 
 
-housing = pd.DataFrame({'house_price':[2, 4, 6, 12], 'houses_sold':[1000, 800, 600, 400]})
 
-
-# n = len(housing['house_price'])
-# print(n)
-
-# function to calculate a sample convariance
-
-# def sample_convariance(x:pd.DataFrame, N) -> int:
-#     for index, data in x.items():
-#         data = pd.DataFrame({'x_i': x['house_price'] - statistics.mean( x['house_price']), 'y_i':x['houses_sold']- statistics.mean(x['houses_sold'])})
-#         return (data.astype(int).sum()/(N-1))
-# 
 
 
 
@@ -29,13 +17,11 @@ def choice(x,y):
     p =  math.factorial(x)/math.factorial(x-y)
     return p
 
-# print(sample_convariance(housing,n))
-#print(housing.cov())
+
 print(purmetation(10,5))
 print(choice(10,5))
 
 
-text = "WORKPLACE SEXUAL HARASSMENT IN THE “ME TOO” ERA: THE UNFORESEEN CONSEQUENCES OF CONFIDENTIAL SETTLEMENT AGREEMENTS"
-print(text.lower())
+
 
 
